98ffa2ac8a7d388ea98e94c2e28a34b6952796c4
Anton Tarasenko <report641@gmail.com>
2022-04-12 04:25:43 +0700

    Начало работы над 12 заданием

--- js/full_size_photo_render.js
+++ js/full_size_photo_render.js
@@ -1,3 +1,12 @@
+import { getArrayOfObjects } from './data.js';
+
+const usersData = getArrayOfObjects(25);
+
 const bigPicture = document.querySelector('.big-picture');
 
 bigPicture.classList.remove('hidden');
+
+usersData.forEach((userData) => {
+
+});
+
